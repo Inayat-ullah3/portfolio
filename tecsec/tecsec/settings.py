a28856d2532957ca03f50d171d3ddd46042feb8c
@inayat-shah <ullah4799992@cloud.neduet.edu.pk>
2025-08-24 15:50:46 +0500

    Set DEBUG=False for production deployment

--- tecsec/tecsec/settings.py
+++ tecsec/tecsec/settings.py
@@ -113,4 +113,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-DEBUG=True
+# DEBUG=True
